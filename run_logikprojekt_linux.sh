bc29d58731abc5666df0c383d89828a85b85dd6d
ChadiABO <chadi@hecatstudio.com>
2025-06-04 21:01:37 +0200

    2025-06-04 update for flame 2026

--- run_logikprojekt_linux.sh
+++ run_logikprojekt_linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /Volumes/RESOURCES/GITHUB/LOGIK-PROJEKT-HECAT
-/opt/Autodesk/python/2025.2.2/bin/python /Volumes/RESOURCES/GITHUB/LOGIK-PROJEKT-HECAT/logik_projekt.py
+/opt/Autodesk/python/2026.0.1/bin/python /Volumes/RESOURCES/GITHUB/LOGIK-PROJEKT-HECAT/logik_projekt.py
